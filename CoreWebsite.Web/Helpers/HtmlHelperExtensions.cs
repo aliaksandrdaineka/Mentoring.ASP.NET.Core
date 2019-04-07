7eeffcc412c8e8d255db7c5bee81fd651c7aa673
daynekoas <daynekoas@gmail.com>
2019-04-08 02:48:50 +0300

    Finished 3.3 - Added html and tag helpers

---
+++ CoreWebsite.Web/Helpers/HtmlHelperExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CoreWebsite.Web.Helpers
+{
+    public static class HtmlHelperExtensions
+    {
+        public static IHtmlContent NorthwindImageLink(this IHtmlHelper helper, int imageId, string linkText)
+        {
+            return new HtmlString($"<a href='/images/{imageId}'>{linkText}</a>");
+        }
+    }
+}
---
+++ CoreWebsite.Web/Helpers/NorthwindImageLinkTagHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreWebsite.Web.Helpers
+{
+    [HtmlTargetElement("a", Attributes = "northwind-id")]
+    public class NorthwindImageLinkTagHelper : TagHelper
+    {
+        public int NorthwindId { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.Add("href", $"/images/{NorthwindId}");
+        }
+    }
+}
